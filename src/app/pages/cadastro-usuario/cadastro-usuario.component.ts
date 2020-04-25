078ad4f3995e65515e8e0d29d8d45c66796bfa7f
Renan Zulian <renanzulian@gmail.com>
2020-04-25 12:39:39 -0300

    Finalizando gerencialmento do formulário

--- src/app/pages/cadastro-usuario/cadastro-usuario.component.ts
+++ src/app/pages/cadastro-usuario/cadastro-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-usuario',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroUsuarioComponent implements OnInit {
 
-  constructor() { }
+  formCadastro: FormGroup
+  get form() { return this.formCadastro.value }
+  constructor() {
+    this.formCadastro = new FormGroup({
+      email: new FormControl('', [Validators.email]),
+      senha: new FormControl('', [Validators.minLength(6)]),
+      nome: new FormControl('', [Validators.minLength(5)]),
+      qtdeLojas: new FormControl('1')
+    })
+   }
 
   ngOnInit() {
+
+  }
+
+  registrar(){
+    console.log(this.form)
   }
 
 }
